5968cb7c61b915625237a79fe010b3f963fe057d
HelloMyNameIsSH <98816434+HelloMyNameIsSH@users.noreply.github.com>
2022-02-01 20:33:52 +0800

    Update 2-5.cpp

--- Accelerated C++/chapter02/2-5.cpp
+++ Accelerated C++/chapter02/2-5.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::cout; using std::cin;
+using std::endl; using std::string;
+
+// Accelerated C++ 
+// book-exercises Chapter 2-5
+// Write by SH, redlegend@163.com
 
 int main() {
-  cout << "**" << endl;
-  cout << "**" << endl << endl;
 
-  cout << "***" << endl;
-  cout << "***" << endl << endl;
+  cout << "Please input rows:";
+  int rows;
+  cin >> rows;
+  
+  cout << "Please input cols:";
+  int cols;
+  cin >> cols;
 
-  cout << "*" << endl;
-  cout << "**" << endl;
+  // triangle  *
+  for (int i = 0; i < rows; i++) {
+    string t = string(i * 2 + 1 , '*');
+    string space = string(rows - i - 1, ' ');
+    cout << space << t << endl;
+  }  
+  cout << endl;
+
+  // square  *
+  for (int i = 0; i < rows; i++) {
+    string t = string(rows, '*');
+    cout << t << endl;
+  }
+  cout << endl;
+
+  // rectangle  *
+  for (int i = 0; i < rows; i++) {
+    string t = string(cols, '*');
+    cout << t << endl;
+  }
 
   return 0;
 }
